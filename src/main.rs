a9c5f47f836c53d2e4b2cd40055733fd59f96801
Alejandro R <alejandro.reyesj@outlook.com>
2023-03-21 18:28:06 -0400

    Antialiasing

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use rustracer::{
+    camera::Camera,
     ray::{self, Hittables},
     sphere::Sphere,
     units::{
-        color,
-        point::{self, Point},
-        vec3::{self, unit_vector},
+        color::{write_color, Color},
+        point::Point,
+        vec3::unit_vector,
     },
 };
 
@@ -13,21 +14,14 @@ fn main() {
     let aspect_ratio = 16.0 / 9.0;
     let image_width = 400;
     let image_height = (image_width as f64 / aspect_ratio) as i32;
+    let samples_per_pixel = 100;
 
     // World
     let mut world = Hittables::new();
     world.add(Box::new(Sphere::new(Point::new(0.0, 0.0, -1.0), 0.5)));
     world.add(Box::new(Sphere::new(Point::new(0.0, -100.5, -1.0), 100.0)));
     // Camera
-    let viewport_height = 2.0;
-    let view_width = aspect_ratio * viewport_height;
-    let focal_length = 1.0;
-
-    let origin = point::Point::new(0.0, 0.0, 0.0);
-    let horizontal = vec3::Vec3::new(view_width, 0.0, 0.0);
-    let vertical = vec3::Vec3::new(0.0, viewport_height, 0.0);
-    let lower_left_corner =
-        origin - horizontal / 2.0 - vertical / 2.0 - vec3::Vec3::new(0.0, 0.0, focal_length);
+    let camera = Camera::new();
 
     // Render
     println!("P3\n{image_width} {image_height}\n255\n");
@@ -35,12 +29,15 @@ fn main() {
     for j in (0..image_height).rev() {
         eprint!("\rScanlines remaining: {j} ");
         for i in 0..image_width {
-            let u = i as f64 / (image_width - 1) as f64;
-            let v = j as f64 / (image_height - 1) as f64;
-            let ray = ray::Ray::new(origin, lower_left_corner + horizontal * u + vertical * v);
-            let color = ray_color(&ray, &world);
+            let mut pixel_color = Color::new(0.0, 0.0, 0.0);
+            for _s in 0..samples_per_pixel {
+                let u = (i as f64 + random_f64()) / (image_width - 1) as f64;
+                let v = (j as f64 + random_f64()) / (image_height - 1) as f64;
+                let ray = camera.get_ray(u, v);
+                pixel_color += ray_color(&ray, &world);
+            }
 
-            color::write_color(color);
+            write_color(pixel_color, samples_per_pixel)
         }
     }
     eprintln!("Done");
@@ -51,13 +48,13 @@ fn random_f64() -> f64 {
     rng.gen()
 }
 
-fn ray_color(r: &ray::Ray, world: &Hittables) -> color::Color {
+fn ray_color(r: &ray::Ray, world: &Hittables) -> Color {
     if let Some(rec) = world.hit(r, 0.0, std::f64::MAX) {
-        return 0.5 * (rec.normal + color::Color::new(1.0, 1.0, 1.0));
+        return 0.5 * (rec.normal + Color::new(1.0, 1.0, 1.0));
     }
     let unit_direction = unit_vector(r.direction());
     let t = 0.5 * (unit_direction.y() + 1.0);
-    color::Color::new(1.0, 1.0, 1.0) * (1.0 - t) + color::Color::new(0.5, 0.7, 1.0) * t
+    Color::new(1.0, 1.0, 1.0) * (1.0 - t) + Color::new(0.5, 0.7, 1.0) * t
 }
 
 // fn hit_sphere(center: point::Point, radius: f64, r: &ray::Ray) -> f64 {
--- src/units/color.rs
+++ src/units/color.rs
@@ -2,9 +2,29 @@ use super::vec3::Vec3;
 
 pub type Color = Vec3;
 
-pub fn write_color(pixel_color: Color) {
-    let x = (255.999 * pixel_color.x()) as i64;
-    let y = (255.999 * pixel_color.y()) as i64;
-    let z = (255.999 * pixel_color.z()) as i64;
-    println!("{x} {y} {z}");
+pub fn write_color(pixel_color: Color, samples_per_pixel: i64) {
+    let mut r = pixel_color.x();
+    let mut g = pixel_color.y();
+    let mut b = pixel_color.z();
+    //Divide color by samples_per_pixel
+    let scale = 1.0 / samples_per_pixel as f64;
+    r *= scale;
+    g *= scale;
+    b *= scale;
+    r *= 255.999;
+    g *= 255.999; // let x = (255.999 * pixel_color.x()) as i64;
+    b *= 255.999;
+    // let y = (255.999 * pixel_color.y()) as i64;
+    // let z = (255.999 * pixel_color.z()) as i64;
+    println!("{} {} {}", r as i64, g as i64, b as i64);
+}
+
+pub fn clamp(x: f64, min: f64, max: f64) -> f64 {
+    if x < min {
+        min
+    } else if x > max {
+        max
+    } else {
+        x
+    }
 }
